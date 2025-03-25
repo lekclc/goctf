b2ca685b7a64cdf45cdae5db32bb09e21f2751c9
lekclc <s2676225006@gmail.com>
2025-03-25 11:34:00 +0800

    fix set

--- src/api/game/set.go
+++ src/api/game/set.go
@@ -2,6 +2,7 @@ package game
 
 import (
 	game_ "src/logic/game"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -14,6 +15,12 @@ func (s *Game) Set(c *gin.Context) {
 	end_time_str := c.PostForm("end")
 	name := c.PostForm("game")
 	desc := c.PostForm("desc")
+	id_ := c.PostForm("id")
+	parsedID, err := strconv.ParseUint(id_, 10, 64)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid ID format"})
+		return
+	}
 	const timeFormat = "2006-01-02T15:04"
 	start_time, err := time.Parse(timeFormat, start_time_str)
 	if err != nil {
@@ -32,7 +39,12 @@ func (s *Game) Set(c *gin.Context) {
 	g.End = end_time
 	g.Name = name
 	g.Desc = desc
-	id, err := g.Set()
+	var id uint
+	if uint(parsedID) != 0 {
+		id, err = g.Set(uint(parsedID))
+	} else {
+		id, err = g.Set()
+	}
 	if err != nil {
 		c.JSON(400, gin.H{"error": err.Error()})
 		return
