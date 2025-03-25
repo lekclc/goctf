4e2143afe3ebadcb6d9ab5f9169b9ac0a8dd28e0
lekclc <s2676225006@gmail.com>
2025-03-25 11:36:08 +0800

    a

--- src/logic/game/set.go
+++ src/logic/game/set.go
@@ -6,19 +6,28 @@ import (
 	"src/database"
 )
 
-func (s *Game) Set() (uint, error) {
+func (s *Game) Set(id ...uint) (uint, error) {
 
 	db := con.Db.Db
 	var g database.Game
 	db.Where("name = ?", s.Name).First(&g)
-	if g.ID != 0 {
-		return 0, errors.New("Game already exists")
-	}
 	g.Name = s.Name
 	g.Start = s.Start
 	g.End = s.End
 	g.Desc = s.Desc
-	err := db.Create(&g).Error
+	var err error
+	if id != nil {
+		if g.ID != id[0] {
+			return 0, errors.New("Game not exists")
+		}
+		err = db.Save(&g).Error
+	} else {
+		if g.ID != 0 {
+			return 0, errors.New("Game already exists")
+		}
+		err = db.Create(&g).Error
+	}
+
 	if err != nil {
 		return 0, err
 	}
